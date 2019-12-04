3f4d9d82184cc5eb921df572d26f837d72fdf128
slfiene <sean.fiene@nintex.com>
2019-12-03 17:00:11 -0800

    updated port log

--- app.js
+++ app.js
@@ -16,15 +16,8 @@ app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    if ('OPTIONS' == req.method) {
-       res.sendStatus(200);
-     }
-     else {
-       next();
-     }});
-     
+     });
+
 routes(app);
 
 app.use((err, req, res, next) => {
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Running on port 3050');
+    console.log('Running on port 3000');
 });
