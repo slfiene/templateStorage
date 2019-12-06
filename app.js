dd184443d9b5b208178bdf62cfb9550593c9ce7c
slfiene <sean.fiene@nintex.com>
2019-12-06 14:06:14 -0800

    updatedCorsOptions

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
 
+var corsOptions = {
+    exposedHeaders: "X-Total-Count"
+}
+
 mongoose.Promise = global.Promise;
 if(process.env.NODE_ENV !== 'test') {
     mongoose.connect('mongodb://templatestorage:0lgqY4f45zJbXHSryKjO1ehO2tlp7u5Ceq2zhAxCAcq5lmhkXgj8LopcsJmi7OoUC1U3WzByrkefrpc2ARKpCg==@templatestorage.mongo.cosmos.azure.com:10255/?ssl=true&appName=@templatestorage@', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -12,7 +16,7 @@ if(process.env.NODE_ENV !== 'test') {
 
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
