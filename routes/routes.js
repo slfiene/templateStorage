6b67eba0cf73df09922104f74af6bb090cdaf53e
Sean <sean.fiene@nintex.com>
2019-12-11 16:16:35 -0800

    fixed bug in routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const TemplatesController = require('../controllers/templates_controller');
 
 module.exports = (app) => {
     app.get('/api/templates', TemplatesController.index);
-    app.get('/api/templates:id', TemplatesController.getOne);
+    app.get('/api/templates/:id', TemplatesController.getOne);
     app.post('/api/templates/create', TemplatesController.create);
     app.put('/api/templates/update/:id', TemplatesController.edit);
     app.delete('/api/templates/:id', TemplatesController.delete);
