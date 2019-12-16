f618fa036b9256d293f3b0d5dc81b6b5a9a5d34b
slfiene <sean.fiene@nintex.com>
2019-12-16 15:31:15 -0800

    update again

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if(process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.json());
 app.use(cors({
     exposedHeaders: ['X-Total-Count'],
-    origin: '*'
+    origin: "*"
 }));
 
 // app.use(function(req, res, next) {
