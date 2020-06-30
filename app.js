aaaa500fb36e04965f122de7646719be449a6cc0
slfiene <sean.fiene@nintex.com>
2020-06-30 08:38:55 -0700

    parseURLEncoded

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ if(process.env.NODE_ENV !== 'test') {
 
 
 app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors({
     exposedHeaders: ['X-Total-Count'],
     origin: "*"
