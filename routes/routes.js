2015c5a1c964fcde7c396e14c593cc45c85752ca
seanFiene <sean@latigreTech.com>
2020-12-13 16:42:58 -0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -8,8 +8,8 @@ module.exports = (app) => {
     // Template based Routes
     app.get('/api/templates', TemplatesController.index);
     app.get('/api/templates/:id', TemplatesController.getOne);
-    app.get('/api/:capability', TemplatesController.getAllOfType);
-    app.get('api/nwc', TemplatesController.getNWCWorkflows);
+    app.get('/api/:capability/', TemplatesController.getAllOfType);
+    app.get('api/nwc/', TemplatesController.getNWCWorkflows);
     app.post('/api/templates/', TemplatesController.create);
     app.put('/api/templates/:id', TemplatesController.edit);
     app.delete('/api/templates/:id', TemplatesController.delete);
